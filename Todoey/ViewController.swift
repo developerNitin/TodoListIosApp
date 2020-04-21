d083d617f83f56bd99ad3c5d27a55acdd67a30a3
Nitin Birdi <nitin.birdi@icloud.com>
2020-04-21 19:57:45 +0530

    #3_commit: tableviewcontroller created and items added.

--- Todoey/ViewController.swift
+++ Todoey/ViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class todoListViewController: UITableViewController {
+    
+    let items = ["cake with cream", "milk of cow", "falling song"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoListCell", for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
 }
 
