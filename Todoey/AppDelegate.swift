2557b20fec566e3fc8589c84cd5284e964b84652
Nitin Birdi <nitin.birdi@icloud.com>
2020-04-26 19:15:57 +0530

    #12_commit: Saving Data with Core Data

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -32,17 +32,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
       // MARK: - Core Data Saving support
 
-      func saveContext () {
-          let context = persistentContainer.viewContext
-          if context.hasChanges {
-              do {
-                  try context.save()
-              } catch {
-                  let nserror = error as NSError
-                  fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-              }
-          }
-      }
-
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }
-
--- Todoey/controller/ViewController.swift
+++ Todoey/controller/ViewController.swift
@@ -1,31 +1,18 @@
 
 import UIKit
+import CoreData
 
 class todoListViewController: UITableViewController {
     
     var items = [Item]()
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-//        let newItem1 = Item()
-//        newItem1.title = "bunburder"
-//        items.append(newItem1)
-//
-//        let newItem2 = Item()
-//        newItem2.title = "sun-bun"
-//        items.append(newItem2)
-//
-//        let newItem3 = Item()
-//        newItem3.title = "find jerry"
-//        items.append(newItem3)
-        
-//        if let itemArray = defaults.array(forKey: "TodoListArray") as? [String] {
-//            items = itemArray
-//        }
-        loadItems()
+//        loadItems()
     }
     
     
@@ -63,10 +50,12 @@ class todoListViewController: UITableViewController {
         
         alert.addAction(UIAlertAction(title: "add item", style: .default) { (action) in
             
-            let newItem = Item()
+           
+            let newItem = Item(context: self.context)
             newItem.title = textFiled.text!
             
             self.items.append(newItem)
+            newItem.done = false
             self.saveItem()
             self.tableView.reloadData()
         })
@@ -81,26 +70,24 @@ class todoListViewController: UITableViewController {
     
     
     func saveItem() {
-       let encoder = PropertyListEncoder()
                    
        do {
-        let data = try encoder.encode(items)
-           try data.write(to: dataFilePath!)
+         try context.save()
        } catch {
-           print("error is \(error)")
+         print("error unresolved (saving context) \(error)")
        }
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            
-            do {
-                items = try decoder.decode([Item].self, from: data)
-            } catch {
-                print(error)
-            }
-        }
-    }
+//    func loadItems() {
+//        if let data = try? Data(contentsOf: dataFilePath!) {
+//            let decoder = PropertyListDecoder()
+//
+//            do {
+//                items = try decoder.decode([Item].self, from: data)
+//            } catch {
+//                print(error)
+//            }
+//        }
+//    }
 }
 
