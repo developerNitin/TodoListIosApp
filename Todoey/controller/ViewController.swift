7cb383aaee357dd12b85d72b858fe738bb878719
Nitin Birdi <nitin.birdi@icloud.com>
2020-04-28 20:54:34 +0530

    #16_commit: Implementing a UISearchBar and Querying with Core Data

--- Todoey/controller/ViewController.swift
+++ Todoey/controller/ViewController.swift
@@ -8,18 +8,16 @@ class todoListViewController: UITableViewController {
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
         loadItems()
     }
     
+    //MARK: - Tableview method
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
-    
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -31,6 +29,7 @@ class todoListViewController: UITableViewController {
         
         return cell
     }
+    
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
@@ -45,6 +44,7 @@ class todoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    //MARK: - Bar Button method
     
     @IBAction func barButton(_ sender: UIBarButtonItem) {
         
@@ -56,8 +56,8 @@ class todoListViewController: UITableViewController {
            
             let newItem = Item(context: self.context)
             newItem.title = textFiled.text!
-            
             self.items.append(newItem)
+            
             newItem.done = false
             self.saveItem()
             self.tableView.reloadData()
@@ -71,9 +71,9 @@ class todoListViewController: UITableViewController {
        present(alert,animated: true, completion: nil)
     }
     
+    //MARK: - Add new items
     
     func saveItem() {
-                   
        do {
          try context.save()
        } catch {
@@ -81,13 +81,27 @@ class todoListViewController: UITableViewController {
         }
     }
     
-    func loadItems() {
-        let element: NSFetchRequest <Item> = Item.fetchRequest()
+    
+    func loadItems(request: NSFetchRequest <Item> = Item.fetchRequest()) {
         do {
-            items = try context.fetch(element)
+            items = try context.fetch(request)
         } catch {
             print("fetch error \(error)")
         }
     }
 }
 
+//MARK: - Search bar method
+
+extension todoListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(request: request)
+        
+        tableView.reloadData()
+    }
+}
